4e2461245f88d3efe7531d4f679f6352e9bf9203
spirosservos <158774236+spirosservos@users.noreply.github.com>
2026-05-11 16:45:32 +0100

    Update streamlit_app_new.py

--- streamlit_app_new.py
+++ streamlit_app_new.py
@@ -14,7 +14,7 @@ except:
     GENAI_KEY = "AIzaSyBhxq6Io1WSNEhb1ZSQMH4LBiQItx9cr1U"
 
 ai_client = genai.Client(api_key=GENAI_KEY)
-AI_MODEL = "gemini-2.0-flash-lite"
+AI_MODEL = "gemini-1.5-flash"
 
 # ==========================================
 # 1. PAGE CONFIGURATION
